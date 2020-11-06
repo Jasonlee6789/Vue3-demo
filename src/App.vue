4502cb1de1340e90b5038936eff2f823f51a58d9
Jasonlee6789 <jeffersonlee@163.com>
2020-11-06 08:55:36 +0800

    我使用了watch来使用了监听一些变化的作用.

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,17 @@
     {{index}}:{{item}}
   </button>
   <div>您选择了--{{selectGirl}}--为你服务</div>
+  <div><button @click="overAction">点餐完毕</button></div>
+  <div>{{overText}}</div>
   </div>
 </template>
 
 <script lang="ts">
 //reactive()
-import { defineComponent, ref, reactive, toRefs } from 'vue';
+import { 
+  defineComponent, ref, reactive, watch,
+  toRefs, onBeforeMount, onMounted,
+  onBeforeUpdate, onRenderTracked, onRenderTriggered} from 'vue';
 
 interface DataProps {
 girls: string[];
@@ -27,6 +32,8 @@ selectGirlFun: (index: number) => void;
 export default ({
   name: 'App',
   setup(){
+    console.log("1-开始创建组件---setup()")
+    //reactive() 函数接收一个对象作为参数，并返回一个代理对象。
     const data: DataProps = reactive({
 girls: ["苍井","三央","爱乃"],
 selectGirl: "",
@@ -34,16 +41,58 @@ selectGirlFun: (index: number)=>{
    data.selectGirl= data.girls[index]
 }
     })
+
+
+// onBeforeMount(()=>{
+//    console.log("2-组件挂在到页面之前执行---onBeforeMount")
+// })
+
+// onMounted(()=>{
+//   console.log("3-组件挂在到页面之后执行---onMounted")
+// })
+
+//  onBeforeUpdate(()=>{
+//     console.log("4-组件更新之前执行---onBeforeUpdate")
+//  })
+
+//  onUpdate(()=>{
+//     console.log("5-组件更新之后执行---onUpdate")
+//  })
+
+// onRenderTracked((event)=>{
+// console.log("散弹枪状态跟踪钩子函数");
+// console.log(event);
+// });
+
+// onRenderTriggered((event)=>{
+// console.log("狙击枪状态跟踪钩子函数");
+// console.log(event);
+// })
 const refData = toRefs(data)
     // const girls = ref(["苍井","三央","爱乃"]);
     // const selectGirl = ref("");
     // const selectGirlFun = (index: number)=>{
     //   selectGirl.value = girls.value[index]
-    // }
+    // } 注意需要通过.value 属性进行获取值。
+//ref 可以把基本的数据结构包装成响应式--即可以直接用到组件中,
+//可以理解为把简单类型的数据包装成引用类型，可以有内存地址的。
+const overText = ref("红浪漫");
+const overAction = ()=>{
+   overText.value = "点餐完成|" + overText.value;
+  // document.title = overText.value;
+}
+watch([overText,()=>data.selectGirl], (newValue,oldValue)=>{
+  console.log(`new---->${newValue}`);
+  console.log(`old---->${oldValue}`);
+  document.title = newValue[0];
+})
     return{
       // data
-  ...refData
+    ...refData,
+    overText,
+    overAction
   }
+  
   }
 });
 </script>
